5bbe4a348758657764dfef0976f843ebed503bd1
apnamcd-ux <apnamcd@gmail.com>
2026-08-24 20:43:18 +0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,17 +18,6 @@ dependencyResolutionManagement {
 rootProject.name = "multi-account-cloner"
 
 include(":app")
-include(":Bcore")
 
-// Map Bcore internal modules to BlackBox repository layout
-include(":Bcore:Bcore")
-project(":Bcore:Bcore").projectDir = file("Bcore/Bcore")
-
-include(":Bcore:black-fake")
-project(":Bcore:black-fake").projectDir = file("Bcore/black-fake")
-
-include(":Bcore:pine:pine-xposed")
-project(":Bcore:pine:pine-xposed").projectDir = file("Bcore/pine/pine-xposed")
-
-include(":Bcore:pine:pine-core")
-project(":Bcore:pine:pine-core").projectDir = file("Bcore/pine/pine-core")
+// Treat the cloned BlackBox repo as an included build
+includeBuild("Bcore")
