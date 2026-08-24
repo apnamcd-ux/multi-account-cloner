0b511c3e8e46bb352600dc33480383e972384e29
apnamcd-ux <apnamcd@gmail.com>
2026-08-24 16:12:20 +0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
 rootProject.name = "multi-account-cloner"
 include(":app")
