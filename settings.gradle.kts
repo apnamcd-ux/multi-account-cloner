d88d52c2838968b3b2efb70494b87a693ad6d689
apnamcd-ux <apnamcd@gmail.com>
2026-08-24 21:33:42 +0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,9 +19,21 @@ rootProject.name = "multi-account-cloner"
 
 include(":app")
 
-includeBuild("Bcore") {
-    dependencySubstitution {
-        // BlackBox defines the submodule as ':Bcore' inside its root project
-        substitute(module("top.niunaijun.blackbox:Bcore")).using(project(":Bcore"))
-    }
-}
+// Directly include BlackBox modules from the cloned Bcore folder
+include(":Bcore")
+project(":Bcore").projectDir = file("Bcore/Bcore")
+
+include(":Bcore:black-fake")
+project(":Bcore:black-fake").projectDir = file("Bcore/Bcore/black-fake")
+
+include(":Bcore:black-hook")
+project(":Bcore:black-hook").projectDir = file("Bcore/Bcore/black-hook")
+
+include(":Bcore:pine-core")
+project(":Bcore:pine-core").projectDir = file("Bcore/Bcore/pine-core")
+
+include(":Bcore:pine-xposed")
+project(":Bcore:pine-xposed").projectDir = file("Bcore/Bcore/pine-xposed")
+
+include(":Bcore:pine-xposed-res")
+project(":Bcore:pine-xposed-res").projectDir = file("Bcore/Bcore/pine-xposed-res")
